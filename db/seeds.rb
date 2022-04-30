551b9eb5a178c095ccd49fe76b603cd44fd94fa5
Ana Arias <athelas85@gmail.com>
2022-04-30 16:29:42 +0100

    almost done, just more front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
+50.times do
   Movie.create(
     title: Faker::Movie.title,
-    overview: Faker::Movies::PrincessBride.quote,
+    overview: Faker::Fantasy::Tolkien.poem,
     poster_url: Faker::LoremFlickr.image,
     rating: rand(0..10)
   )
