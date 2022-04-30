a1ad438e4d73a319b5072f8a5c0a156b97844ef3
Ana Arias <athelas85@gmail.com>
2022-04-30 12:12:30 +0100

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Movies.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movies::PrincessBride.quote,
+    poster_url: Faker::LoremFlickr.image,
+    rating: rand(0..10)
+  )
+end
